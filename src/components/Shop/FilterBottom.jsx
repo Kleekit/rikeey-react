10d4cbf56ccec7ec43dea979d72d1dadcd0d5e03
Amayo <mayo16collins@gmail.com>
2021-11-09 12:30:30 +0100

    nov 8th [cole]

---
+++ src/components/Shop/FilterBottom.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import { getCategory } from "../../methods/product.method";
+
+export default function FilterBottom() {
+  const { data } = useQuery("getCategory", getCategory);
+
+  return (
+    <span>
+      {data &&
+        data.status &&
+        data.body.map((body) => (
+          <div
+            className="filterBarLinks mx-auto w-60"
+            key={body.category.categoryName}
+          >
+            <Link to="/">
+              <div className="filter-mn fs-nm fw-700 mb-3 w-70 px-2">
+                {body.category.categoryName}
+              </div>
+            </Link>
+            <div className="subCat mt-3">
+              {body.subCategories.length > 0 &&
+                body.subCategories.map((subCat) => (
+                  <Link to="/" key={subCat.subCategoryName}>
+                    <h4 className="mb-3 ms-2">{subCat.subCategoryName}</h4>
+                  </Link>
+                ))}
+            </div>
+          </div>
+        ))}
+    </span>
+  );
+}
