40fb3378c2188674b0bf446aff606ee7ce7e91eb
Martins Ngene <martinsngene@gmail.com>
2022-01-05 13:20:18 +0100

    [update]

--- src/components/Utility/ProductCard.jsx
+++ src/components/Utility/ProductCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ProductCard({ children }) {
   return (
-    <div className="productContainer flex flex-col justify-center px-[1.4%] w-1/2 sm:w-1/4">
+    <div className="productContainer flex flex-col justify-center px-[1.4%] w-1/2 sm:w-1/4 my-5">
       <div className="productImg ">
         <img
           className="w-full rounded-[2rem] h-[55vh] "
@@ -15,6 +15,8 @@ export default function ProductCard({ children }) {
   );
 }
 
-ProductCard.Name = (props) => <p>{props.children}</p>;
+ProductCard.Name = (props) => <p className="my-3 text-3xl">{props.children}</p>;
 
-ProductCard.Price = (props) => <p>{props.children}</p>;
+ProductCard.Price = (props) => (
+  <p className="my-3 font-bold text-3xl">{props.children}</p>
+);
