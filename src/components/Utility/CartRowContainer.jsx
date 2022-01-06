d68966959de0cb6433ad49b77edae87c580e131f
Amayo <mayo16collins@gmail.com>
2022-01-06 11:01:01 +0100

    conflict resolved @zac

--- src/components/Utility/CartRowContainer.jsx
+++ src/components/Utility/CartRowContainer.jsx
@@ -76,16 +76,10 @@ export default function CartRowContainer({ items, refetch }) {
       </div>
       <div className="border-y-[0.1rem] border-[#5E6368] py-[4rem] pl-[8rem]">
         {/* <SingleCartRow item={item} refetch={refetch} /> */}
-<<<<<<< HEAD
-        {/* <SingleCartRow />
-        <SingleCartRow />
-        <SingleCartRow /> */}
-=======
         {items.map((item) => (
           // <SingleCartRow />
           <SingleCartRow item={item} refetch={refetch} key={item.productName} />
         ))}
->>>>>>> 7acfe5612049fc6f7fb487ceede4146c91f17725
       </div>
     </div>
   );
