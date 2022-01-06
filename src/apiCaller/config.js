0d64cb86575ca996a05d1848a0ccfe24162bf42b
ekamanelly <ekamanelly@gmail.com>
2022-01-06 16:45:49 +0100

    :building_construction: still changing

--- src/apiCaller/config.js
+++ src/apiCaller/config.js
@@ -1,5 +1,5 @@
 export const config = {
-  baseurl: true ? "https://rikeey.com/api/v1" : "http://localhost:2727/api/v1",
+  baseurl: false ? "https://rikeey.com/api/v1" : "http://localhost:2727/api/v1",
   authHeader:
     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImVrYW1hIiwiX2lkIjoiNjExZmI0NzA2ZjNiMjYxZGQ0MWVmNDQyIiwiZW1haWwiOiJla2FtYW5lbGx5QGdtaWFsLmNvbSIsImlhdCI6MTYyOTQ2NzgxOX0.iBdzdXMQOvp6bm5YrV-YdLh8ypP0C8o4dPCa5M2D99o",
 };
